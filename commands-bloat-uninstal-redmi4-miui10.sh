f7c1efafe21d0b54c54e4e1f636b1bcdf87534a9
Asharam <seerviasharam@gmail.com>
2019-11-02 16:53:41 +0530

    Initial commit

---
+++ commands-bloat-uninstal-redmi4-miui10.sh
@@ -0,0 +1,52 @@
+#Direct Commands written for Redmi4 (MIUI10) (Nougat)
+#May vary as per your version of MIUI & Android
+
+#Misc
+pm uninstall -k --user 0 com.cmcm.indianews_for_oem
+pm uninstall -k --user 0 com.dsi.ant.server
+pm uninstall -k --user 0 com.newsdog
+pm uninstall -k --user 0 com.swiftkey.languageprovider
+pm uninstall -k --user 0 com.swiftkey.swiftkeyconfigurator
+pm uninstall -k --user 0 com.touchtype.swiftkey
+
+#Xiaomi
+pm uninstall -k --user 0 com.xiaomi.glgm
+pm uninstall -k --user 0 com.xiaomi.joyose
+pm uninstall -k --user 0 com.xiaomi.mipicks
+pm uninstall -k --user 0 com.xiaomi.payment
+
+#MIUI
+pm uninstall -k --user 0 com.miui.analytics
+pm uninstall -k --user 0 com.miui.android.fashiongallery
+pm uninstall -k --user 0 com.miui.bugreport
+pm uninstall -k --user 0 com.miui.hybrid
+pm uninstall -k --user 0 com.miui.msa.global
+pm uninstall -k --user 0 com.miui.player
+pm uninstall -k --user 0 com.miui.userguide
+pm uninstall -k --user 0 com.miui.videoplayer
+pm uninstall -k --user 0 com.miui.weather2
+pm uninstall -k --user 0 com.miui.yellowpage
+
+#Mi
+pm uninstall -k --user 0 com.mi.android.globalpersonalassistant
+pm uninstall -k --user 0 com.mi.dlabs.vr
+pm uninstall -k --user 0 com.mi.global.bbs
+pm uninstall -k --user 0 com.mi.global.shop
+pm uninstall -k --user 0 com.mipay.wallet.in
+
+#Google
+pm uninstall -k --user 0 com.google.android.feedback
+pm uninstall -k --user 0 com.google.android.gm
+pm uninstall -k --user 0 com.google.android.music
+pm uninstall -k --user 0 com.google.android.videos
+
+#Android
+pm uninstall -k --user 0 com.android.browser
+pm uninstall -k --user 0 com.android.chrome
+pm uninstall -k --user 0 com.android.thememanager
+pm uninstall -k --user 0 com.android.wallpaper.livepicker
+
+#Facebook
+pm uninstall -k --user 0 com.facebook.appmanager
+pm uninstall -k --user 0 com.facebook.services
+pm uninstall -k --user 0 com.facebook.system
---
+++ uninstall-miui-bloat-script.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+adb devices
+read -p "If using ADB for first time, grant permission on your phone and press Ok."
+
+uninstall=(
+com.cmcm.indianews_for_oem
+com.dsi.ant.server
+com.newsdog
+com.swiftkey.languageprovider
+com.swiftkey.swiftkeyconfigurator
+com.touchtype.swiftkey
+com.xiaomi.glgm
+com.xiaomi.joyose
+com.xiaomi.mipicks
+com.xiaomi.payment
+com.miui.analytics
+com.miui.android.fashiongallery
+com.miui.bugreport
+com.miui.hybrid
+com.miui.msa.global
+com.miui.player
+com.miui.userguide
+com.miui.videoplayer
+com.miui.weather2
+com.miui.yellowpage
+com.mi.android.globalpersonalassistant
+com.mi.dlabs.vr
+com.mi.global.bbs
+com.mi.global.shop
+com.mipay.wallet.in
+com.google.android.feedback
+com.google.android.gm
+com.google.android.music
+com.google.android.videos
+com.android.browser
+com.android.chrome
+com.android.thememanager
+com.android.wallpaper.livepicker
+com.facebook.appmanager
+com.facebook.services
+com.facebook.system
+)
+
+for X in ${uninstall[@]}
+do
+    echo $X
+    adb shell pm uninstall $X
+    adb shell pm uninstall --user 0 $X
+done
+
+pause
